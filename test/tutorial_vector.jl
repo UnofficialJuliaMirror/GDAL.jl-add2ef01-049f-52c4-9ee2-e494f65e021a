JuliaGeo/GDAL.jl

add tests that need GEOS
Should help finding build issues with GEOS earlier.
Now it seems that the linux builds don't have a working GEOS install: https://travis-ci.org/yeesian/ArchGDAL.jl/jobs/587690528#L356

Expression: AG.isring(point) == false
GDALError (CE_Failure, code 6):
  GEOS support not enabled.

test/tutorial_vector.jl
@@ -72,6 +72,8 @@ feature = GDAL.ogr_f_create(featuredefn)
 GDAL.ogr_f_setfieldstring(feature, GDAL.ogr_f_getfieldindex(feature, "Name"), "myname")
 point = GDAL.ogr_g_creategeometry(GDAL.wkbPoint)
 GDAL.ogr_g_setpoint_2d(point, 0, 100.123, 0.123)
+@test GDAL.ogr_g_isvalid(point) == 1
+@test GDAL.ogr_g_isring(point) == 0
 @test GDAL.ogr_f_setgeometry(feature, point) == GDAL.OGRERR_NONE
 GDAL.ogr_g_destroygeometry(point)
 GDAL.ogr_l_createfeature(layer, feature)

UnofficialJuliaMirror/GDAL.jl-add2ef01-049f-52c4-9ee2-e494f65e021a

(cherry picked from commit 36a0df0fd065194228345020e51e815091b0b648)

test/tutorial_vector.jl
@@ -72,6 +72,8 @@ feature = GDAL.f_create(featuredefn)
 GDAL.setfieldstring(feature, GDAL.getfieldindex(feature, "Name"), "myname")
 point = GDAL.creategeometry(GDAL.wkbPoint)
 GDAL.setpoint_2d(point, 0, 100.123, 0.123)
+@test GDAL.isvalid(point) == 1
+@test GDAL.isring(point) == 0
 @test GDAL.setgeometry(feature, point) == GDAL.OGRERR_NONE
 GDAL.destroygeometry(point)
 